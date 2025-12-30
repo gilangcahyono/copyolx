92761ebd7839f360e688b20faa39b190e937581e
Gilang Cahyono <gilcah2207@gmail.com>
2025-12-30 23:57:20 +0700

    prog: register page

--- pages/register.jsx
+++ pages/register.jsx
@@ -2,18 +2,60 @@ import AuthLayout from "@/components/layout/AuthLayout";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import { toast } from "sonner";
 
 const Register = () => {
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const router = useRouter();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    toast.success("Account created successfully");
+    router.push("/login");
+  };
   return (
     <AuthLayout>
-      <div className="flex flex-col gap-3 w-full">
+      <form className="flex flex-col gap-3 w-full" onSubmit={handleSubmit}>
         <h1 className="text-center text-2xl font-bold">
           Register your account
         </h1>
-        <Input type="text" placeholder="Name" />
-        <Input type="text" placeholder="Username" />
-        <Input type="password" placeholder="Password" />
-        <Input type="password" placeholder="Confirm Password" />
+        <Input
+          type="text"
+          placeholder="Name"
+          value={name}
+          name="name"
+          onChange={(e) => setName(e.target.value)}
+          required
+        />
+        <Input
+          type="text"
+          placeholder="Username"
+          required
+          value={username}
+          name="username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          required
+          value={password}
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Confirm Password"
+          required
+          value={confirmPassword}
+          name="confirmPassword"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
         <Button variant="">Register</Button>
         <span>
           Already have an account ?{" "}
@@ -21,7 +63,7 @@ const Register = () => {
             Login
           </Link>
         </span>
-      </div>
+      </form>
     </AuthLayout>
   );
 };
